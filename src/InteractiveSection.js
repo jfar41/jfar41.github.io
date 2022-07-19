b95d77fad74ca1ca46b64656112fe1cae1839edb
jfar41 <68749301+jfar41@users.noreply.github.com>
2022-07-19 10:41:33 -0700

    Set up lights

--- src/InteractiveSection.js
+++ src/InteractiveSection.js
@@ -6,50 +6,10 @@ import {OrbitControls, useHelper} from '@react-three/drei'
 import { SpotLightHelper } from "three";
 import { Scene } from "three";
 
-import { Lamp } from "./components/lights";
+import { Lamp1, Lamp2, Lamp3 } from "./components/lights";
 
 const seaTexture = new THREE.TextureLoader().load(require("./images/sandGrey.jpg"));
 
-const AquaLight = () => {
-    let tgtObj = (<mesh rotation={[-Math.PI/2, 0, 10]} position={[-50, 200, 0]}>
-        <planeBufferGeometry args={[10,10]} />
-        <meshPhongMaterial color="red" side={THREE.DoubleSide}/>
-    </mesh>)
-    const helper = useRef();
-    const spotLightTgtTile = useRef();
-    useHelper(helper, SpotLightHelper, "#1B5E6C")
-    return <spotLight
-        ref={helper}
-        intensity={1}
-        position={[-50, 200,0]}
-        angle={0.174533}
-        target-position={[-50, 200, 0]}
-    />
-    const group = <group>
-        <spotLight 
-        ref={helper} 
-        intensity={1} 
-        position={[0, 200, 0]} 
-        castShadow={true} 
-        angle={0.174533}
-        />
-        <mesh
-            rotation={[-Math.PI/2, 0, 10]}
-            position={[-40, -50, -10]}
-            ref={spotLightTgtTile}
-        >
-            <planeBufferGeometry args={[10, 10]} />
-            <meshPhongMaterial
-                color="red"
-                side={THREE.DoubleSide}
-                // map={seaTexture}
-            />
-        </mesh>
-    </group>
-
-    return group
-   
-}
 export class InteractiveSection extends React.Component {
     constructor(props) {
         super(props);
@@ -85,8 +45,10 @@ export class InteractiveSection extends React.Component {
                         // enableZoom={false}
                     />
                     {/* <AquaLight /> */}
-                    <ambientLight />
-                    <Lamp position={[0, 15, 0]}/>
+                    {/* <ambientLight /> */}
+                    <Lamp1 position={[0, 15, 0]}/>
+                    <Lamp2 />
+                    <Lamp3 />
                 </Canvas>
             </div>
         )
